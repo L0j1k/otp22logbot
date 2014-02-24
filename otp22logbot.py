caa11a903159a9ac7b6c84f54050f4ab96233422
L0j1k <L0j1k@L0j1k.com>
2014-02-24 01:50:50 -0600

    corrected input parser

--- otp22logbot.py
+++ otp22logbot.py
@@ -26,6 +26,12 @@ parser.add_argument('-i', '--init',
   nargs='?',
   type=argparse.FileType('r')
 )
+parser.add_argument('-k', '--kill',
+  help='Kill password to stop bot. Default "killme"',
+  default='killme',
+  nargs='?',
+  type=str
+)
 parser.add_argument('-n', '--nick',
   help='IRC nick name. Default "otp22logbot"',
   default='otp22logbot',
@@ -44,7 +50,7 @@ parser.add_argument('-p', '--port',
   nargs='?',
   type=int
 )
-parser.add_argument('-r', '--realname',
+parser.add_argument('-r', '--real',
   help='IRC real name. Default "otp22logbot"',
   default='otp22logbot',
   nargs='?',
@@ -56,7 +62,7 @@ parser.add_argument('-s', '--server',
   nargs='?',
   type=str
 )
-parser.add_argument('-u', '--username',
+parser.add_argument('-u', '--user',
   help='IRC user name. Default "otp22logbot"',
   default='otp22logbot',
   nargs='?',
@@ -74,6 +80,7 @@ def process_command( command ):
 
 command_exit = False
 app_data = {
+  'kill': False,
   'overlord': 'L0j1k',
   'version': '0.0.1_prealpha'
 }
@@ -96,25 +103,29 @@ sock.connect((app_args.server, app_args.port))
 #if (app_args.password != False):
 #  sock.send(bytes('PASS '+app_args.password+'\r\n'), 'utf-8')
 sock.send(bytes('NICK '+app_args.nick+'\r\n', 'utf-8'))
-sock.send(bytes('USER '+app_args.username+' '+app_args.server+' default :'+app_args.realname+'\r\n', 'utf-8'))
+sock.send(bytes('USER '+app_args.user+' '+app_args.server+' default :'+app_args.real+'\r\n', 'utf-8'))
 sock.send(bytes('JOIN #'+app_args.channel+'\r\n', 'utf-8'))
 sock.send(bytes('PRIVMSG '+app_data['overlord']+': Greetings, overlord. I am for you.\r\n', 'utf-8'))
 
-while app_data.kill == False:
+while app_data['kill'] == False:
   sock_buffer = sock.recv(1024).decode('utf-8')
   sysprint(sock_buffer)
   if (sock_buffer.find('PING') != -1):
     sock.send(bytes('PONG '+sock_buffer.split()[1]+'\n', 'utf-8'))
   if (sock_buffer.find('PRIVMSG') != -1):
     message = sock_buffer.split(':')
-    message_header = message[0].split()
-    message_body = message[1].split()
+    message_header = message[1].split()
+    message_body = message[2].split()
     this_command = message_body[0]
     this_extra = message_body[1]
     this_requester = message_header[1]
-    if (this_command.find('@version') != -1):
-      sock.send(bytes('PRIVMSG '+this_requester+': \n', 'utf-8'))
     if (this_command.find('@help') != -1):
       sock.send(bytes('PRIVMSG '+this_requester+': \n', 'utf-8'))
+    if (this_command.find('@kill') != -1):
+      if (this_extra == 'killme'):
+        app_data['kill'] = True
+        sock.send(bytes('PRIVMSG '+this_requester+': With urgency, my lord. Dying at your request.\n', 'utf-8'))
     if (this_command.find('@last') != -1):
+      sock.send(bytes('PRIVMSG '+this_requester+': \n', 'utf-8'))
+    if (this_command.find('@version') != -1):
       sock.send(bytes('PRIVMSG '+this_requester+': \n', 'utf-8'))
